1e194374a69a9884526ca760b7a1e32558979e69
J K <kj688895@gmail.com>
2020-09-14 18:22:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,13 @@
 speak_to_grandma = "I love you grandma!".upcase
 
-if speak_to_grandma == "I love you grandma!".upcase
-  puts "I love you too pumpkin!".upcase
-elsif speak_to_grandma == "Hi Nana,how are you?"
-  puts "Huh?! Speak Up, Sonny!".upcase
+if speak_to_grandma == "Hi Nana, how are you?"
+  puts "Huh?! speak up, sonny!".upcase
 elsif speak_to_grandma == "Hi!"
   puts "Huh?! Speak Up, Sonny!".upcase
+elsif speak_to_grandma == "What did you eat today?".upcase
+  puts "No, not since 1938!".upcase
+elsif speak_to_grandma == "I love you grandma!".upcase
+  puts "I love you too pumpkin!".upcase
 else
   puts "No, Not Since 1938!".upcase
 end
