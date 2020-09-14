896b93d09f1764b6cfa216def3b58b00351ba9ca
J K <kj688895@gmail.com>
2020-09-14 18:32:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,6 @@ if speak_to_grandma == "".upcase
   puts "no, not since 1938!".upcase
 elsif speak_to_grandma == "I love you grandma!".upcase
   puts "I love you too pumpkin".upcase
-else 
+else
   puts "Huh?! speak up, sonny!".upcase
 end
