9d691bdf50cb1786a265d740dedec24ffe69b8a8
J K <kj688895@gmail.com>
2020-09-14 18:36:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,1 @@
-speak_to_grandma = "I love you grandma!".upcase
-
-if speak_to_grandma == "#{.upcase}"
-  puts "no, not since 1938!".upcase
-elsif speak_to_grandma == "I love you grandma!".upcase
-  puts "I love you too pumpkin".upcase
-else
-  puts "Huh?! speak up, sonny!".upcase
-end
+speak_to_grandma = "I love you grandma!".upcase"
