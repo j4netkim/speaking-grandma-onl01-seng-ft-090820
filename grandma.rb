d9c4cf1f407216d1444ebeb2108c851c284d6040
J K <kj688895@gmail.com>
2020-09-14 18:14:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
-speak_to_grandma = "Hi Nana, how are you?"
 
 if speak_to_grandma == "I love you grandma!".upcase
   puts "I love you too pumpkin!".upcase
 elsif speak_to_grandma == "Hi Nana,how are you?"
+  puts "Huh?! Speak Up, Sonny!".upcase
+elseif speak_to_grandma == "Hi!"
   puts "Huh?! Speak Up, Sonny!".upcase
 else
   puts "No, Not Since 1938!".upcase
