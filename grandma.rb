d088603a9a367afc1deb80237a24e4440064af9b
J K <kj688895@gmail.com>
2020-09-14 18:53:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,1 @@
-speak_to_grandma = "I love you grandma!".upcase
-
-  if speak_to_grandma == "I LOVE YOU GRANDMA!"
-    puts "I love you too pumpkin!".upcase
-  elsif speak_to_grandma == phrase.upcase
-    puts "no, not since 1938!".upcase
-  else
-    puts "huh?! speak up, sonny!".upcase
-end
-
-
-speak_to_grandma("I LOVE YOU GRANDMA!")
+def speak_to_grandma(phrase)
